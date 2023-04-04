db1f114cba2ee0e79b21107960f02dde839d8cd2
Gustavo José Lemos Mendes <gustavo.mendes@w-security.intranet>
2023-04-04 12:59:26 -0300

    Continuando a desenvolver a interface

--- src/components/UserPhoto.tsx
+++ src/components/UserPhoto.tsx
@@ -1,29 +1,47 @@
-import { Image, IImageProps, Center } from 'native-base';
+import { Image, Center, IImageProps, useTheme } from 'native-base';
 import React from 'react';
 import { PencilSimpleLine } from 'phosphor-react-native';
+import { StyleSheet, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { THEME } from '../theme';
 
-type Props = IImageProps & {}
+type Props = IImageProps & {
+}
 
 export function UserPhoto({ size, ...rest }: Props) {
+  const { colors } = THEME;
+
   return (
-    <Center>
-      <Image 
+    <Center
+      w='24'
+      py='1'
+    >
+      <Image
         w={size}
         h={size}
+        alt='Foto de perfil de usuário'
         rounded='full'
         borderWidth={2}
         bg='blue.light'
-        { ...rest }
+        {...rest}
       />
-      <Center 
-        w='10' 
-        h='10' 
-        rounded='full' 
-        bg='blue.light' 
-        position='absolute' 
+      <TouchableOpacity
+        activeOpacity={0.9}
+        style={[styles.button, { backgroundColor: colors.blue.light }]}
       >
-        <PencilSimpleLine color='white'/>
-      </Center>
+        <PencilSimpleLine color='white' size={18} />
+      </TouchableOpacity>
     </Center>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+  }
+})
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,5 +1,5 @@
-import { Box, Text, VStack, Button as NativeBaseButton, Center } from 'native-base';
-import React from 'react';
+import { Box, Text, VStack, Button as NativeBaseButton, Center, ScrollView } from 'native-base';
+import React, { useEffect } from 'react';
 import LogoSvg from '../assets/logo.svg';
 import { UserPhoto } from '../components/UserPhoto';
 import userPhotoPng from '../assets/avatar.png';
@@ -11,41 +11,53 @@ import { Button } from '../components/Button';
 - fazer função de visualizar senha 
 - implementar validações nos campos dos inputs
 - implementar navegação
+- adicionar aquele scroll quando abre o treclado
+- ver pq esse background n ta funfando
 */}
 
 export function SignUp() {
+  useEffect(() => {
+    console.log("SignUp'")
+  }, [])
+
   return (
-    <VStack w='full' h='full' bg='gray.600'>
-      <Center>
-        <LogoSvg width={80} height={80}/>
-        <Text fontSize='20' fontFamily='heading' color='gray.100'>
-          Boas vindas!  
-        </Text>
-        <Text fontSize='14' fontFamily='body' color='gray.200'>
-          Crie sua conta e use o espaço para comprar {'\n'} itens variados e vender seus produtos
-        </Text>
-      </Center>
-      <Box mt='8' flex={1} px='12' >
-        <UserPhoto 
-          source={userPhotoPng}
-          alt='Foto de perfil de usuário'
-        />
-
-        <Input placeholder='Nome'/>
-        <Input placeholder='E-mail'/>
-        <Input placeholder='Telefone'/>
-        <Input placeholder='Senha' secureTextEntry/>
-        <Input placeholder='Confirmar senha' secureTextEntry/>
-
-        <Button type='blue' title='Criar' />
-
-        <Center flex={1}>
-          <Text color='gray.200' alignSelf='center' fontFamily='body' fontSize='14'>
-            Já tem acesso?
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg='gray.600'>
+        <Center>
+          <LogoSvg width={80} height={80} />
+          <Text fontSize='20' fontFamily='heading' color='gray.100'>
+            Boas vindas!
           </Text>
-          <Button type='gray' title='Ir para o login'/>
+          <Text fontSize='14' fontFamily='body' color='gray.200'>
+            Crie sua conta e use o espaço para comprar {'\n'} itens variados e vender seus produtos
+          </Text>
         </Center>
-      </Box>
-    </VStack>
+        <Box mt={6} flex={1} px='12' >
+          <Center>
+            <UserPhoto
+              source={userPhotoPng}
+            />
+          </Center>
+
+          <Input placeholder='Nome' />
+          <Input placeholder='E-mail' />
+          <Input placeholder='Telefone' />
+          <Input placeholder='Senha' secureTextEntry />
+          <Input placeholder='Confirmar senha' secureTextEntry />
+
+          <Button type='black' title='Criar' />
+
+          <Center flex={1}>
+            <Text color='gray.200' alignSelf='center' fontFamily='body' fontSize='14'>
+              Já tem uma conta?
+            </Text>
+            <Button type='gray' title='Ir para o login' />
+          </Center>
+        </Box>
+      </VStack>
+    </ScrollView>
   );
 }
