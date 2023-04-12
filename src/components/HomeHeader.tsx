7157d7a34e6b7d58aac8bb6aa3a35c2cfbb1c814
GustaJLemos <gustavolemosmendes@gmail.com>
2023-04-11 22:37:43 -0300

    Criando o componente

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,34 @@
+import { HStack, Text } from "native-base";
+import { UserPhoto } from "./UserPhoto";
+import userPng from "../assets/user.png";
+import { Button } from "./Button";
+
+export function HomeHeader() {
+  return (
+    <HStack w='full' alignItems='center'>
+      <UserPhoto
+        source={userPng}
+        size='60'
+        showPencil={false}
+      />
+      <Text
+        color='gray.100'
+        fontSize='16'
+        fontFamily='body'
+        ml='2'
+        flex={1}
+      >
+        Boas vindas, {'\n'}
+        <Text
+          color='gray.100'
+          fontSize='16'
+          fontFamily='heading'
+        >
+          Gustavo!
+        </Text>
+      </Text>
+      {/* TODO colocar icone nesse botão */}
+      <Button type="black" title="Criar anúncio" iconName="add" flex={1} />
+    </HStack>
+  );
+}
