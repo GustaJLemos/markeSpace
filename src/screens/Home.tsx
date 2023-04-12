9e37dbbc191f6231e6d78bc14c23fc99a4e202c0
GustaJLemos <gustavolemosmendes@gmail.com>
2023-04-11 22:37:24 -0300

    Mudando alguns estilos

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,36 +1,24 @@
-import { HStack, Text, VStack } from "native-base";
-import { UserPhoto } from "../components/UserPhoto";
-import userPng from "../assets/user.png";
+import { Box, HStack, IIconProps, Text, VStack } from "native-base";
 import { Button } from "../components/Button";
+import { HomeHeader } from "../components/HomeHeader";
+import { AdvertisedProducts } from "../components/advertisedProducts";
 
 export function Home() {
 	return (
 		<VStack w='full' h='full' bg='gray.600' p='6'>
-			<HStack w='full' alignItems='center'>
-				<UserPhoto
-					source={userPng}
-					size='60'
-					showPencil={false}
-				/>
-				<Text
-					color='gray.100'
-					fontSize='16'
-					fontFamily='body'
-					ml='2'
-					flex={1}
-				>
-					Boas vindas, {'\n'}
-					<Text
-						color='gray.100'
-						fontSize='16'
-						fontFamily='heading'
-					>
-						Gustavo!
-					</Text>
-				</Text>
-				{/* TODO colocar icone nesse botão */}
-				<Button type="black" title="Criar anúncio" flex={1} />
-			</HStack>
+			<HomeHeader />
+
+
+			<Text
+				color='gray.300'
+				fontSize='14'
+				fontFamily='body'
+				mt='8'
+				mb='3'
+			>
+				Seus produtos anunciados para venda
+			</Text>
+			<AdvertisedProducts />
 		</VStack>
 	);
 }
