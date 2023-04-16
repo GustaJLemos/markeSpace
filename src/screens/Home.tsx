34b31b64d7479a025085feb827dde5cd7fdc400e
GustaJLemos <gustavolemosmendes@gmail.com>
2023-04-16 13:58:56 -0300

    Usando o componente de card, e adicionando a flat list

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,49 @@
-import { Box, HStack, IIconProps, Text, VStack } from "native-base";
+import { Box, Divider, FlatList, HStack, IIconProps, Text, VStack } from "native-base";
 import { Button } from "../components/Button";
 import { HomeHeader } from "../components/HomeHeader";
 import { AdvertisedProducts } from "../components/AdvertisedProducts";
 import { InputSearch } from "../components/InputSearch";
+import { CardForSale } from "../components/CardForSale";
+import { CardForSaleType } from "../types/CardForSale";
+
+const EXAMPLE_PRODUCTS: CardForSaleType[] = [
+	{
+		id: '1',
+		title: 'Tenis',
+		price: '10,00',
+		category: 'used'
+	},
+	{
+		id: '2',
+		title: 'Jorge',
+		price: '25,00',
+		category: 'new'
+	},
+	{
+		id: '3',
+		title: 'Julieta',
+		price: '48,00',
+		category: 'used'
+	},
+	{
+		id: '4',
+		title: 'Tenis',
+		price: '38,00',
+		category: 'new'
+	},
+	{
+		id: '5',
+		title: 'Jorge',
+		price: '25,00',
+		category: 'new'
+	},
+	{
+		id: '6',
+		title: 'Julieta',
+		price: '918,00',
+		category: 'used'
+	},
+]
 
 export function Home() {
 	return (
@@ -30,6 +71,19 @@ export function Home() {
 				Compre produtos variados
 			</Text>
 			<InputSearch />
+
+			{/* TODO ver a parada de suavizar as bordas de cima da flat lsit */}
+			<FlatList
+				data={EXAMPLE_PRODUCTS}
+				renderItem={({ item }) => (
+					<CardForSale product={item} />
+				)}
+				contentContainerStyle={{ paddingBottom: 60 }}
+				showsVerticalScrollIndicator={false}
+				horizontal={false}
+				numColumns={2}
+				mt='6'
+			/>
 		</VStack>
 	);
 }
