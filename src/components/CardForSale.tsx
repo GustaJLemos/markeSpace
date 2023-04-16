eb1053d3511042bf5a1b768f793f5a4ae555ec3b
GustaJLemos <gustavolemosmendes@gmail.com>
2023-04-16 13:58:16 -0300

    Terminando de criar o componente

--- src/components/CardForSale.tsx
+++ src/components/CardForSale.tsx
@@ -1,11 +1,60 @@
-import { Box, Image, Text, VStack } from "native-base";
+import { TouchableOpacity } from 'react-native';
+import { Box, HStack, Image, Text, VStack } from "native-base";
+import { UserPhoto } from "./UserPhoto";
+import SaleImgExPng from '../assets/saleImgEx.png';
+import UserExPng from '../assets/user.png';
+import { CardForSaleType } from "../types/CardForSale";
 
-export function CardForSale() {
+// TODO colocar as tipagens
+type Props = {
+  product: CardForSaleType
+}
+
+export function CardForSale({ product: { title, price, category } }: Props) {
   return (
-    <VStack>
+    <TouchableOpacity
+      activeOpacity={0.8}
+      style={{
+        marginBottom: 24,
+        marginRight: 48
+      }}
+    >
       <Box>
         {/* TODO colocar imagem aq */}
-        <Image />
+        <Image
+          source={SaleImgExPng}
+          alt="Imagem do usuário anuciante"
+          rounded='md'
+        />
+        <HStack
+          position='absolute'
+          justifyContent='space-between'
+          w='full'
+          px='1'
+        >
+          <UserPhoto
+            size='7'
+            source={UserExPng}
+            showPencil={false}
+          />
+          <Box
+            h='5'
+            rounded='full'
+            justifyContent='center'
+            bg={category === 'new' ? 'blue.light' : 'gray.200'}
+            mt='1'
+            px='2'
+          >
+            <Text
+              color='gray.700'
+              fontSize='10'
+              fontFamily='heading'
+              textTransform='uppercase'
+            >
+              {category === 'new' ? 'novo' : 'usado'}
+            </Text>
+          </Box>
+        </HStack>
         {/* Todo criar os componentes de perfil e de usado */}
       </Box>
 
@@ -14,7 +63,7 @@ export function CardForSale() {
         fontSize='14'
         fontFamily='body'
       >
-        Tênis vermelho
+        {title}
       </Text>
 
       <Text
@@ -28,9 +77,9 @@ export function CardForSale() {
           fontSize='16'
           fontFamily='heading'
         >
-          59,90
+          {price}
         </Text>
       </Text>
-    </VStack>
+    </TouchableOpacity>
   )
 }
