3536fc60ee06ecd4dee2053f7c64177381d49214
GustaJLemos <gustavolemosmendes@gmail.com>
2023-04-16 16:15:29 -0300

    Adicionando o gesture handler

--- src/contexts/Providers/index.tsx
+++ src/contexts/Providers/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NativeBaseProvider } from 'native-base';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { THEME } from '../../theme';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 type Props = {
   children: React.ReactNode
@@ -10,11 +11,13 @@ type Props = {
 export function Providers({ children }: Props) {
   return (
     <SafeAreaProvider style={{ flex: 1 }}>
-      <NativeBaseProvider theme={THEME}>
-        <SafeAreaView>
-          {children}
-        </SafeAreaView>
-      </NativeBaseProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <NativeBaseProvider theme={THEME}>
+          <SafeAreaView>
+            {children}
+          </SafeAreaView>
+        </NativeBaseProvider>
+      </GestureHandlerRootView>
     </SafeAreaProvider>
   );
 }
