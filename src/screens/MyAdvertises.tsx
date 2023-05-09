b33a935bee519d65b4e145efe569401c9342931e
GustaJLemos <gustavolemosmendes@gmail.com>
2023-05-08 22:39:50 -0300

    Criando um select

--- src/screens/MyAdvertises.tsx
+++ src/screens/MyAdvertises.tsx
@@ -1,19 +1,62 @@
-import { Box, FlatList, VStack } from "native-base";
+import { Box, FlatList, HStack, Text, VStack, Select, useTheme } from "native-base";
 import { Header } from "../components/Header";
 import { EXAMPLE_PRODUCTS } from "./Home";
 import { CardForSale } from "../components/CardForSale";
+import SelectDropdown from 'react-native-select-dropdown'
+import { MaterialIcons } from '@expo/vector-icons';
+
+const ADVERTISE_OPTIONS = ['Todos', 'Novos', 'Usados', 'Favoritos']
 
 export function MyAdvertises() {
+  const { colors } = useTheme();
+
   return (
-    <VStack w='full' h='full' bg='gray.600'>
+    <VStack w='full' h='full' bg='gray.700'>
       <Header
         goBack={false}
         title="Meus anúncios"
         rightIcon={{ iconName: 'add', onIconPress: () => console.log('adicionar anuncio') }}
       />
 
-      {/* TODO fazer negócio aq de núnciosn */}
-      <Box px='6'>
+      <Box px='6' py='2'>
+        <HStack
+          w='full'
+          justifyContent='space-between'
+          alignItems='center'
+        >
+          <Text
+            color='gray.200'
+            fontFamily='body'
+            fontSize='14'
+          >
+            9 Anúncios
+          </Text>
+
+          <SelectDropdown
+            data={ADVERTISE_OPTIONS}
+            defaultValue={ADVERTISE_OPTIONS[0]}
+            onSelect={(selectedItem, index) => {
+              console.log(selectedItem, index)
+            }}
+            buttonTextAfterSelection={(selectedItem, index) => {
+              return selectedItem
+            }}
+            rowTextForSelection={(item, index) => {
+              return item
+            }}
+            renderDropdownIcon={() => {
+              return <MaterialIcons
+                name="keyboard-arrow-down"
+                size={24}
+                color={colors.gray[300]}
+              />
+            }}
+            dropdownStyle={{ borderRadius: 8 }}
+            buttonTextStyle={{ color: colors.gray[300] }}
+            buttonStyle={{ backgroundColor: colors.gray[700], borderWidth: 1, borderRadius: 16, borderColor: colors.gray[500] }}
+          />
+        </HStack>
+
         <FlatList
           data={EXAMPLE_PRODUCTS}
           renderItem={({ item }) => (
