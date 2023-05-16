0191e6cb17a74e57139536c20d5e7186edaca2b5
GustaJLemos <gustavolemosmendes@gmail.com>
2023-05-15 21:53:09 -0300

    Terminando de desenvolver a interface

--- src/screens/MyProductDetails.tsx
+++ src/screens/MyProductDetails.tsx
@@ -6,6 +6,8 @@ import UserImagePng from '../assets/user.png';
 import { CategoryTag } from "../components/CategoryTag";
 import { PaymentOptions } from "../components/PaymentOptions";
 import { Button } from "../components/Button";
+import { Tag } from 'phosphor-react-native'
+import { AntDesign } from '@expo/vector-icons';
 
 export function MyProductDetails() {
   const { colors } = THEME;
@@ -35,102 +37,91 @@ export function MyProductDetails() {
           É assim que seu produto vai aparecer!
         </Text>
       </VStack>
-      <VStack h='full' bg='gray.600'>
-        <Image
-          source={MyProductImagePng}
-          alt='Imagem do produto'
-          style={{ width: '100%' }}
-        />
+      <Image
+        source={MyProductImagePng}
+        alt='Imagem do produto'
+        style={{ width: '100%' }}
+      />
+      <VStack h='full' px='4' bg='gray.600'>
 
-        {/* TODO fazer funcionar essa scroll... */}
-        <ScrollView px='6' contentContainerStyle={{ marginTop: 12, marginBottom: 56 }}>
-          <HStack alignItems='center' justifyContent='center' mb='6'>
-            <UserPhoto
-              source={UserImagePng}
-              showPencil={false}
-              size='7'
-            />
-            <Text
-              color='gray.100'
-              fontSize='14'
-              fontFamily='body'
-              ml='1'
-              numberOfLines={1}
-              flex={1}
-            >
-              Gustavo Mendes
-            </Text>
-          </HStack>
-          <CategoryTag category="new" w='12' />
-
+        {/* TODO colocar uma scrollView funcional */}
+        <HStack alignItems='center' justifyContent='center' mb='6'>
+          <UserPhoto
+            source={UserImagePng}
+            showPencil={false}
+            size='7'
+          />
           <Text
             color='gray.100'
-            fontSize='20'
-            fontFamily='heading'
-            numberOfLines={1}
-            my='2'
-          >
-            Bicicleta
-          </Text>
-          <Text
-            color='gray.200'
             fontSize='14'
             fontFamily='body'
-            mb='2'
-          >
-
-            Cras congue cursus in tortor sagittis placerat nunc, tellus arcu. Vitae ante leo eget maecenas urna mattis cursus. Mauris metus amet nibh mauris mauris accumsan, euismod. Aenean leo nunc, purus iaculis in aliquam.
-            Cras congue cursus in tortor sagittis placerat nunc, tellus arcu. Vitae ante leo eget maecenas urna mattis cursus. Mauris metus amet nibh mauris mauris accumsan, euismod. Aenean leo nunc, purus iaculis in aliquam.
-          </Text>
-
-          <Text
-            color='gray.200'
-            fontSize='14'
-            fontFamily='heading'
+            ml='1'
             numberOfLines={1}
-            my='2'
+            flex={1}
           >
-            Aceita troca?
-            <Text fontFamily='body'> Sim</Text>
+            Gustavo Mendes
           </Text>
+        </HStack>
+        <CategoryTag category="new" w='12' />
 
-          <Text
-            color='gray.200'
-            fontSize='14'
-            fontFamily='heading'
-            numberOfLines={1}
-            my='2'
-          >
-            Meios de pagamento:
-          </Text>
-          <PaymentOptions option="boleto" />
-          <PaymentOptions option="pix" />
-          <PaymentOptions option="dinheiro" />
-          <PaymentOptions option="credito" />
-          <PaymentOptions option="deposito" />
-        </ScrollView>
+        <Text
+          color='gray.100'
+          fontSize='20'
+          fontFamily='heading'
+          numberOfLines={1}
+          my='2'
+        >
+          Bicicleta
+        </Text>
+        <Text
+          color='gray.200'
+          fontSize='14'
+          fontFamily='body'
+          mb='2'
+        >
+
+          Cras congue cursus in tortor sagittis placerat nunc, tellus arcu. Vitae ante leo eget maecenas urna mattis cursus. Mauris metus amet nibh mauris mauris accumsan, euismod. Aenean leo nunc, purus iaculis in aliquam.
+        </Text>
+
+        <Text
+          color='gray.200'
+          fontSize='14'
+          fontFamily='heading'
+          numberOfLines={1}
+          my='2'
+        >
+          Aceita troca?
+          <Text fontFamily='body'> Sim</Text>
+        </Text>
+
+        <Text
+          color='gray.200'
+          fontSize='14'
+          fontFamily='heading'
+          numberOfLines={1}
+          my='2'
+        >
+          Meios de pagamento:
+        </Text>
+        <PaymentOptions option="boleto" />
+        <PaymentOptions option="pix" />
 
         <HStack
-          bg='gray.700'
-          w='full'
-          px='6'
-          pb='4'
-          justifyContent='space-between'
-          alignItems='center'
+          space='2'
         >
           <Button
-            type="blue"
+            type="gray"
             title="Voltar e editar"
-            // icon={<WhatsappLogo size={20} weight='fill' color={colors.gray[600]} />}
-            w='48'
+            icon={<AntDesign name="arrowleft" size={18} color={colors.gray[200]} />}
+            w='48%'
             alignSelf='center'
           />
 
           <Button
             type="blue"
             title="Publicar"
-            // icon={<WhatsappLogo size={20} weight='fill' color={colors.gray[600]} />}
-            w='48'
+            icon={<Tag size={18} color={colors.gray[600]} />}
+            w='48%'
             alignSelf='center'
           />
         </HStack>
